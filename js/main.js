e1ed32dbd7d079086eebc16d150255823691a356
aoi <5cats.ussct@gmail.com>
2025-11-27 23:17:43 +0900

    卒業生の実績

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".slide-items").slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: "100px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed:3000 ,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: "50px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
